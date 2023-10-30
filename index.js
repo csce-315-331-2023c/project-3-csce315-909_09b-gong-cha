998716ecec46c5b0111e75476fbe48d6015218b2
Brenndan Croteau <brenndancroteau@tamu.edu>
2023-10-30 17:34:48 -0500

    Added functionality to link html files to specific web pages

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('static'));
+
+// This is used to link an HTML page to the respective page of our website, home being '/'
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+    res.sendFile(__dirname + '/public/index.html');
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
