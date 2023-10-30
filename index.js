fa874b7e1926046cfa100aade3c960707fe8fae1
AntonAsus <noki.ramirez@gmail.com>
2023-10-30 18:15:20 -0500

    Added query functionality

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://csce315_909_antonhugo1:password@csce-315-db.engr.tamu.edu:5432/csce315331_09b_db')
+
+const query = pgp.as.format('select * from recipe')
+console.log(query)
+db.any(query).then((data) => {
+  app.get('/', (req, res) => {
+    res.send(JSON.stringify(data[60]));
+  });
+  console.log(data);
+
+
+//   for (var key in data) {
+//     if (data.hasOwnProperty(key)) {
+//         console.log(key + " -> " + data[key]);
+//     }
+// }
+
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
