c417668a55fa1c374208150552a0ec7adb0e98dc
lan <mai.lauren2@gmail.com>
2023-10-31 15:30:16 -0500

    added some pages, navbar too

---
+++ static/cashier.js
@@ -0,0 +1,14 @@
+function Translate(){
+
+}
+
+//request to database to return all recipes
+//dynamically create buttons for each recipe returned
+//add event listener to each button
+//when clicked, display recipe in the recipe div
+//display ingredients in the ingredients div
+//display instructions in the instructions div
+
+function makeRecipeButtons(){
+    
+}
