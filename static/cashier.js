bdfe380d5a7698d9f7937f7be1fa2441dd24fad0
lan <mai.lauren2@gmail.com>
2023-11-01 23:58:00 -0500

    huge overhaul, api is sort of working now, and dynamically generated buttons

--- static/cashier.js
+++ static/cashier.js
@@ -1,59 +1,26 @@
-// class Recipe{
-//     constructor(recipe_id, recipe_name, is_slush, med_price, large_price, recipe_price){
-//         this.recipe_id = recipe_id;
-//         this.recipe_name = recipe_name;
-//         this.is_slush = is_slush;
-//         this.med_price = med_price;
-//         this.large_price = large_price;
-//         this.recipe_price = recipe_price;
-//     }
-// }
+url = "http://localhost:5000"; //changes this later
 
-class RecipeButton{
-    //when clicked, i want it to add to the recipe div
-
-
-}
-
-//request to database to return all recipes
-//dynamically create buttons for each recipe returned
-//add event listener to each button
-async function ReadRecipes() {
-    var json = await fetch(url + "/recipe", {method:"GET"}).then((res) => res.json());
-    console.log(json);
-    return json;
-}
-
-async function makeRecipeButtons(){
-    //get all recipes from database
-    var recipes = await ReadRecipes(); //recipe is a json
-    //we insert depending on tabcontent
-    var milkTea = document.getElementById("milktea");
-    var slushie = document.getElementById("slushie");
-    var coffee = document.getElementById("coffee");
-    var other = document.getElementById("other");
-
-    for(var i = 0; i < recipes.length; i++){
-        var button = document.createElement("button");
-        button.innerHTML = recipes[i].recipe_name;
-        button.id = recipes[i].recipe_id;
-        button.className = "RecipeButton";
-        if(recipes[i].is_slush){
-            slushie.appendChild(button);
-        }
-        else if(recipes[i].recipe_name.includes("milk")){
-            milkTea.appendChild(button);
-        }
-        else if(recipes[i].recipe_name.includes("coffee")){
-            coffee.appendChild(button);
-        }
-        else{
-            other.appendChild(button);
-        }
-    }
+//onload for body, run makeRecipeButtons
+document.body.onload = function(){insertinfo()};
     //add event listeners to each button
     //when clicked, display recipe in the recipe div
     //display ingredients in the ingredients div
     //display instructions in the instructions div
 
+async function insertinfo(){
+    var request = await fetch(url + "/recipe").then((res) => res.json());
+    console.log(request);
+    console.log("from insertinfo");
+    var milkTea = document.getElementById("milktea");
+    var slushie = document.getElementById("slushie");
+    var coffee = document.getElementById("coffee");
+    var other = document.getElementById("other");
+
+    //insert the json into the div
+    for (var i = 0; i < request.length; i++){
+        var button = document.createElement("button");
+        button.innerHTML = request[i].recipe_name;
+        button.className = "RecipeButton";
+        milkTea.appendChild(button);
+    }
 }
