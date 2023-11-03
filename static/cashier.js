b063b57393868c25d78d94354d7c1232c9bd7e1a
lan <mai.lauren2@gmail.com>
2023-11-03 12:31:35 -0500

    some small format changes to cashier.js

--- static/cashier.js
+++ static/cashier.js
@@ -2,15 +2,16 @@ url = "http://localhost:5000"; //changes this later
 
 //onload for body, run makeRecipeButtons
 document.body.onload = function(){insertinfo()};
-    //add event listeners to each button
-    //when clicked, display recipe in the recipe div
-    //display ingredients in the ingredients div
-    //display instructions in the instructions div
 
+    /**
+     * add event listeners to each button
+     * when clicked, display recipe in the recipe div
+     * display ingredients in the ingredients div
+     * display instructions in the instructions div
+     * */
 async function insertinfo(){
     var request = await fetch(url + "/recipe").then((res) => res.json());
-    console.log(request);
-    console.log("from insertinfo");
+
     var milkTea = document.getElementById("milktea");
     var slushie = document.getElementById("slushie");
     var coffee = document.getElementById("coffee");
