00fd23494623cff34c3bdc71c652472a2d7448da
Brenndan Croteau <brenndancroteau@tamu.edu>
2023-11-06 19:23:28 -0600

    Added json to table function

--- static/manager.js
+++ static/manager.js
@@ -236,3 +236,64 @@ async function modIngredientStock() {
   const msg = await response.text();
   console.log(msg);
 }
+
+// Generate table that will hold all the ingredients
+async function ingredientTable() {
+  var request = await fetch(url + "/manager/ingredients").then((res => res.json()));
+  console.log(request)
+  createTableFromJSON(request, 'ingredient-table')
+}
+
+// Generate table that will hold all the drinks
+async function drinkTable() {
+  var request = await fetch(url + "/recipe").then((res => res.json()));
+  console.log(request)
+  createTableFromJSON(request, 'drink-table')
+}
+
+// Make a table given json data 
+async function createTableFromJSON(jsonData, tableContainerID) {
+  // Get the container element where the table will be inserted
+  let container = document.getElementById(tableContainerID);
+  if (container.childElementCount > 0)
+     container.removeChild(container.firstChild);
+
+  // Clear the container's contents
+  container.innerHTML = '';
+
+  // Create the table element
+  let table = document.createElement("table");
+
+  // Get the keys (column names) of the first object in the JSON data
+  let cols = Object.keys(jsonData[0]);
+
+  // Create the header element
+  let thead = document.createElement("thead");
+  let tr = document.createElement("tr");
+
+  // Loop through the column names and create header cells
+  cols.forEach((item) => {
+     let th = document.createElement("th");
+     th.innerText = item; // Set the column name as the text of the header cell
+     tr.appendChild(th); // Append the header cell to the header row
+  });
+  thead.appendChild(tr); // Append the header row to the header
+  table.append(tr) // Append the header to the table
+
+  // Loop through the JSON data and create table rows
+  jsonData.forEach((item) => {
+     let tr = document.createElement("tr");
+
+     // Get the values of the current object in the JSON data
+     let vals = Object.values(item);
+
+     // Loop through the values and create table cells
+     vals.forEach((elem) => {
+        let td = document.createElement("td");
+        td.innerText = elem; // Set the value as the text of the table cell
+        tr.appendChild(td); // Append the table cell to the table row
+     });
+     table.appendChild(tr); // Append the table row to the table
+  });
+  container.appendChild(table) // Append the table to the container element
+}
