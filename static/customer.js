3ca5b773ade41aa337aac2a1c5cd204a944ba7aa
lan <mai.lauren2@gmail.com>
2023-11-19 19:57:40 -0600

    dynamic buttons

--- static/customer.js
+++ static/customer.js
@@ -1,4 +1,67 @@
+const url = 'http://localhost:5000';
+
 function ToggleVis(divName_vis, divName_hide){
     divName_hide.style.display = "none";
     divName_vis.style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    insertinfo();
+});
+
+function createButton(drinkname, json) {
+    var button = document.createElement("button");
+    // <button type="button" class="btn btn-light btn-outline-dark btn-lg"><img src="images/teas/1.png">Milk Tea Series</button>
+    
+    button.type = "button";
+    button.className = "btn btn-light btn-outline-dark btn-lg";
+    const image = document.createElement('img');
+    image.src = 'images/teas/' + json.recipe_id + '.png';
+    
+    button.textContent = drinkname;
+    button.appendChild(image);
+
+    button.addEventListener("click", function() {
+        console.log(json);
+        // <button type="button" class="btn btn-light btn-outline-dark btn-lg p-3 m-3" onclick="ToggleVis(buttons, milkDiv)">Back</button>
+        //change visibility of current div to hidden
+        // ToggleVis();
+    });
+    return button;
+}
+
+async function insertinfo(){
+    var request = await fetch(url + "/recipe").then((res) => res.json());
+    
+    var milkTea = document.getElementById("milktea");
+    var slushie = document.getElementById("slush");
+    var coffee = document.getElementById("coffee");
+    var tea = document.getElementById("tea");
+    var other = document.getElementById("other");
+    //clear all the divs
+    milkTea.innerHTML = "";
+    slushie.innerHTML = "";
+    coffee.innerHTML = "";
+    tea.innerHTML = "";
+    other.innerHTML = "";
+    //insert the json into the div
+    for (var i = 0; i < request.length; i++){
+        button = createButton(request[i].recipe_name, request[i]);
+        let name = request[i].recipe_name;
+        if(request[i].is_slush){
+            slushie.appendChild(button);
+        }
+        else if(name.includes("Coffee")){
+            coffee.appendChild(button);
+        }
+        else if(name.includes("Milk")){
+            milkTea.appendChild(button);
+        }
+        else if(name.includes("Tea")){
+            tea.appendChild(button);
+        }
+        else{
+            other.appendChild(button);
+        }
+    }
 }
