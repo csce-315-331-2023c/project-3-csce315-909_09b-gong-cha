17e291a746ea953b9ad42abf2cbe1afd9c73495b
Reid Jenkins <reid_jenkins@tamu.edu>
2023-11-29 19:03:33 -0600

    created weather app on about page

--- static/about.js
+++ static/about.js
@@ -1,3 +1,23 @@
+const url = 'http://localhost:5000';
+
 $('.carousel').carousel({
     interval: 5000 //have it cycle
   })
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const weatherInfo = document.getElementById('weather-info');
+
+  fetch(url + `/weather`)
+    .then((response) => response.json())
+    .then((data) => {
+      const { current } = data;
+      const tempF = current.temp_f;
+
+      weatherInfo.innerHTML = `Weather: ${tempF}°F`;
+    })
+    .catch((error) => {
+      console.error('Error fetching weather data:', error);
+      weatherInfo.innerHTML = 'Weather: Not available';
+    });
+});
