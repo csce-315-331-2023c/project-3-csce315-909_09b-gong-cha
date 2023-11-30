5c1e16092f176c3499956c25beba4ea687f060dd
lan <mai.lauren2@gmail.com>
2023-11-29 23:44:42 -0600

    accessibility sizing

---
+++ static/textch.js
@@ -0,0 +1,21 @@
+// Function to change text size
+function changeTextSize(size) {
+    $('body').css('font-size', size);
+
+    // Save the user's preference in a cookie
+    document.cookie = `textSize=${size}; path=/`;
+    }
+
+    // Retrieve the user's preferred text size from the cookie on page load
+    function getSavedTextSize() {
+    const size = document.cookie.replace(/(?:(?:^|.*;\s*)textSize\s*=\s*([^;]*).*$)|^.*$/, "$1");
+    if (size) {
+        changeTextSize(size);
+        $('#text-size').val(size);
+    }
+    }
+
+    // Call the function on page load
+    $(document).ready(function () {
+    getSavedTextSize();
+    });
